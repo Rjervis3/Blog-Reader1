984b5683c55c209e0ba0e2b0f6ba8af403382186
rjervis33@me.com <jervisre@grinnell.edu>
2014-10-22 13:47:08 -0500

    Initial Commit

---
+++ Blog Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blog Reader
+//
+//  Created by Renn Jervis on 10/22/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blog Reader/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Blog Reader
+//
+//  Created by Renn Jervis on 10/22/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
